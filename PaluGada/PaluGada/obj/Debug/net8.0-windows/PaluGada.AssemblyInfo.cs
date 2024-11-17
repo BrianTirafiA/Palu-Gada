134f2b09161b952ef882adba806ebc8c14085b42
BrianTirafiA <briantirafiaa@gmail.com>
2024-11-17 18:09:31 +0700

    -

--- PaluGada/PaluGada/obj/Debug/net8.0-windows/PaluGada.AssemblyInfo.cs
+++ PaluGada/PaluGada/obj/Debug/net8.0-windows/PaluGada.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaluGada")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a43ea845b6bd644440ad527e7b2d7dfca493f586")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaluGada")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaluGada")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
